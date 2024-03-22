0920a86d778a1499c15e4ccca42eaaed50cd6067
Md Mustaqim Alam <mustaqimalam85@gamil.com>
2024-03-22 16:19:52 +0530

    New Commit

---
+++ src/Components/Completed/CompletedTodos.jsx
@@ -0,0 +1,7 @@
+
+
+function CompletedTodos() {
+  return <div></div>;
+}
+
+export default CompletedTodos;
---
+++ src/Components/Todostatus.jsx
@@ -0,0 +1,14 @@
+
+function Todostatus() {
+  return (
+    <div className=' bg-red-800 w-full h-11 mb-3 flex items-center justify-around '>
+    <link rel="stylesheet"
+     href=""
+      />
+        <button > Pending </button>
+        <button> Completed </button>
+    </div>
+  )
+}
+
+export default Todostatus
